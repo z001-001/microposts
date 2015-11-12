c27e241319dd304c59579ec989eceb87ed18828f
z001_001 <z001-001@drmscp.net>
2015-11-12 12:13:58 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,27 @@ class User < ActiveRecord::Base
   validates :url, length: { maximum: 255 }
   has_secure_password
   has_many :microposts
+  has_many :following_relationships, class_name: "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent: :destroy
+  has_many :following_users, through: :following_relationships, source: :followed
+  has_many :follower_relationships, class_name: "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent: :destroy
+  has_many :follower_users, through: :follower_relationships, source: :follower
+
+  # 他のユーザーをフォローする
+  def follow(other_user)
+    following_relationships.create(followed_id: other_user.id)
+  end
+
+  # フォローしているユーザーをアンフォローする
+  def unfollow(other_user)
+    following_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # あるユーザーをフォローしているかどうか？
+  def following?(other_user)
+    following_users.include?(other_user)
+  end
 end
