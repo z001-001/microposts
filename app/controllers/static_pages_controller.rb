680b3c5ee7d7ea8a8068f3e495df88258acda025
z001_001 <z001-001@drmscp.net>
2015-11-16 11:57:06 +0000

    add home view's timeline [retake]

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if logged_in?
-    @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+    end
   end
 end
